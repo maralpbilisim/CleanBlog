af2cc913f86436b3b3f5beac6f8ba68ab1605240
maralpbilisim <72076365+maralpbilisim@users.noreply.github.com>
2022-08-28 17:41:48 +0300

    son hali

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const { request } = require('express');
-const exprees = require('express');
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const app = express();
 
-const app = exprees();
+//VİEW ENGİNE
+app.set('view engine', 'ejs');
 
-app.get('/',(req,res)=>{
-    const blog = { 
-        id: 1, 
-        title: "Blog title", 
-        description: "Blog description" 
-    }
-  res.send(blog)
-})
+//MİDDLEWARE
+app.use(express.static('public'));
 
-const port = 3000;
+//ROUTES
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+const port = 4000;
 app.listen(port, () => {
-  console.log(`sunucu ${port}unda başlatıldı`);
+  console.log(`Server ${port} portunda dinleniyor`);
 });
